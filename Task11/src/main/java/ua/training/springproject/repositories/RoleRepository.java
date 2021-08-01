d17032c644afa36b76cc32ec5da09770b62fa804
OleksiiYermosin <80970354+OleksiiYermosin@users.noreply.github.com>
2021-08-01 20:28:15 +0300

    Delete Task11 directory

--- Task11/src/main/java/ua/training/springproject/repositories/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package ua.training.springproject.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ua.training.springproject.entities.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
--- Task11/src/main/java/ua/training/springproject/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package ua.training.springproject.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ua.training.springproject.entities.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
